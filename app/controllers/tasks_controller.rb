6395eea247c062753698cd634360f8e157cc5ca4
jeff-ma <jefm206@gmail.com>
2017-02-26 02:50:58 +0000

    Made changes to controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,42 @@
 class TasksController < ApplicationController
+	before_action :find_task, only: [:show, :edit, :update, :destroy]
+
 	def index
+		@task = Task.all("created_at DESC"
 	end
+
+	def show
+	end
+
+	def new
+		@task = Task.new
+	end
+
+	def edit
+	end
+
+	def create
+		@task = Task.new(task_params)
+		if @task.save
+			redirect_to_ root_path
+		else
+			render 'new'
+		end
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+	private
+		def task_params
+			params.require(:task).permit(:title, :text)
+		end
+
+		def find_task
+			@task = Task.find(params[:id])
+		end
+
 end
